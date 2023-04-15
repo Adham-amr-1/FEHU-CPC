01c326080b963e577aedb631f5cbf4ea11e70c40
Adham Amr <104816206+Adham-amr-1@users.noreply.github.com>
2023-04-15 22:09:10 +0200

    Create A - Plus or Minus.cpp

---
+++ A - Plus or Minus.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c, t;
+	cin >> t;
+	while (t--) {
+		cin >> a >> b >> c;
+		if (a + b == c) {
+			cout << '+' << endl;
+		}
+		else if (a - b == c) {
+			cout << '-' << endl;
+		}
+	}
+	return 0;
+}
